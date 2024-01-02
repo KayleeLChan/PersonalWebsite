6881c31097d1d2e4d93344c1721fed9912c2a355
Kaylee Chan <kaylee.chan@mail.utoronto.ca>
2024-01-01 22:55:37 -0800

    POC working

--- website/src/pages/Home.jsx
+++ website/src/pages/Home.jsx
@@ -1,14 +1,46 @@
 import React from 'react';
+import {Parallax, ParallaxLayer} from '@react-spring/parallax'
 
 function Home() {
 
     return (
-        <>
-        <h1>
-            Hello world!
-        </h1>
-        <i class="bi bi-backpack4-fill"></i>
-        </>
+        <Parallax pages={9} className="parallax">
+            <ParallaxLayer
+                offset={0}
+                sticky={{ start:0, end: 3 }}
+                className="avatar"
+            >
+                <img src="/Avatar.svg" width="300" height="300" />
+            </ParallaxLayer>
+            <ParallaxLayer
+                offset={1}
+                sticky={{ start:1, end: 2 }}
+            >
+                <div className="d-flex flex-column justify-content-start">
+                    <img src="/Laptop.svg" className="item" width="150" height="150"/>
+                </div>
+            </ParallaxLayer>
+            <ParallaxLayer
+                offset={1.7}
+                speed={1.5}
+            >
+                <p>I am a programmer!</p>
+            </ParallaxLayer>
+            <ParallaxLayer
+                offset={2}
+                sticky={{ start:2, end: 3 }}
+            >
+                <div className="d-flex flex-column justify-content-start">
+                    <img src="/UofT.svg" className="item" width="150" height="150"/>
+                </div>
+            </ParallaxLayer>
+            <ParallaxLayer
+                offset={2.7}
+                speed={1.5}
+            >
+                <p>I go to uoft!</p>
+            </ParallaxLayer>
+        </Parallax>
     );
 }
 
