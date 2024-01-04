28408a54a121456a631144b8c0b483eb083531c5
Kaylee Chan <kaylee.chan@mail.utoronto.ca>
2024-01-03 23:53:52 -0800

    additional padding

--- website/src/pages/Contact.jsx
+++ website/src/pages/Contact.jsx
@@ -9,7 +9,7 @@ function Contact() {
                 <h1>Contact Me</h1>
                 <img src="Smile.svg" width="150" height="150" className="h-flip" alt="Kaylee Chan pixel avatar smiling" />
             </div>
-            <div className="d-flex flex-column justify-content-center align-items-center mt-3 gap-3">
+            <div className="d-flex flex-column justify-content-center align-items-center mt-3 p-3 gap-3">
                 <div className="d-flex flex-row text-primary-white gap-2 justify-content-center align-items-center">
                     <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-envelope" viewBox="0 0 16 16">
                         <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1zm13 2.383-4.708 2.825L15 11.105zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741M1 11.105l4.708-2.897L1 5.383z" />
