bff837ca8c581096121b85c1f3b4abb1f4ecf3e3
Kaylee Chan <kaylee.chan@mail.utoronto.ca>
2024-02-21 21:46:07 -0500

    added image links and styling

--- website/src/components/cardCarousel.jsx
+++ website/src/components/cardCarousel.jsx
@@ -23,13 +23,15 @@ const CardCarousel = ({ entries }) => {
                 {entries.map((entry) => (
                     <SwiperSlide
                         key={entry.title}
-                        className={`rounded-3 p-3 ${entry.link ? 'clickable-slide' : ''}`}
+                        className={`rounded-3 ${entry.link ? 'clickable-slide' : ''} ${entry.imageLink ? 'image-slide' : ''}`}
                         // If entry has an external link, open it in a new tab on click
                         onClick={() => entry.link && window.open(entry.link, '_blank')}
                         aria-label={entry.link ? `${entry.link} (opens in new tab)` : ""}
                         rel={entry.link ? "noopener noreferrer" : ""}
+                        // If entry has an image, set the slide's background image to it
+                        style={entry.imageLink ? { backgroundImage: `url(${entry.imageLink})` } : {}}
                     >
-                        <div className="d-flex flex-column justify-content-start align-items-center">
+                        <div className="d-flex flex-column justify-content-center align-items-center h-100 w-100 p-3">
                             <h2>{entry.title}</h2>
                             <h3>{entry.start} {entry.end && <> &mdash; {entry.end}</>}</h3>
                             <p>{entry.content}</p>
